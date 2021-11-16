144eb517dcdb35e045f83e284735863e24196ad3
chino_desu <pdxxlzh@126.com>
2021-11-16 10:17:17 +0800

    more evil things

--- encode.py
+++ encode.py
@@ -13,18 +13,28 @@ if __name__ == "__main__":
     except:
         print(traceback.format_exc())
 
-    segments = list(reversed(split_seg(code)))
-    payload = ""
+    encodeTime = input("please specify encode times(default 1, enter to use default: ")
+    if encodeTime == "":
+        encodeTime = 1
+    else:
+        try:
+            encodeTime = int(encodeTime)
+        except:
+            print("input error, using default")
+            encodeTime = 1
 
-    #for seg in segments:
-    #    payload = b64obfuscate(seg) + ';' + payload
-    payload = b64obfuscate(code)
+    for i in range(0, encodeTime):
+        if (i == 0):
+            payload = b64obfuscate(code)
+        else:
+            payload = b64obfuscate(payload)
 
     #filePath = input("please specify output file(file path supported)\n")
     filePath = "out.py"
 
     #apply imports
     payload = "import base64;" + payload
+
     try:
         with open(filePath, 'w') as handle:
             handle.write(payload)
